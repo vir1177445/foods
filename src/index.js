e64c306a1cb1e483e2bf1e5741f913b4a5f6edf7
vir1177445 <64118457+vir1177445@users.noreply.github.com>
2020-10-15 13:39:18 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>bacon</li>
+      <li>noodles</li>
+      <li>mushrooms</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
